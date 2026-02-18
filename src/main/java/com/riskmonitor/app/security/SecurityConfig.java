7227928f060f38f8fadbb6bf198a3c76f0df63e9
aryandeshpande11 <aryan.deshpande29@gmail.com>
2026-02-18 20:29:33 +0530

    fix CORS for vercel and allow root path

--- src/main/java/com/riskmonitor/app/security/SecurityConfig.java
+++ src/main/java/com/riskmonitor/app/security/SecurityConfig.java
@@ -33,14 +33,15 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login").permitAll()
                         .requestMatchers("/api/auth/register").permitAll()
                         .requestMatchers("/api/auth/test").permitAll()
-                        .anyRequest().authenticated()
-                )
+                        .requestMatchers("/").permitAll() // ADD THIS
+                        .requestMatchers("/error").permitAll() // ADD THIS
+                        .anyRequest()
+                        .authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
@@ -60,7 +61,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://financial-risk-analyzer-barclay-gqp49wzzq.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
